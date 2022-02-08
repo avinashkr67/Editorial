f621052fde13661446e4d219f036cac802811586
Avinash Kumar <official.avinashkr@gmail.com>
2022-02-08 11:30:28 +0530

    Final Upload

---
+++ js/home.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".menu-bar").click(function(){
+        $(".sidebar").toggle();
+    })
+
+    $(".sm-icon1").click(function(){
+        $(".submenu-list-ul-1").toggle();
+    })
+
+    $(".sm-icon2").click(function(){
+        $(".submenu-list-ul-2").toggle();
+    })
+})
